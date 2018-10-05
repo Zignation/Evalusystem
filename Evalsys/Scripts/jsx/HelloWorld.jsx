53fdf1599f0f0b1d3ba1368ff80033e4324d03d1
ZIGNATION\Søren <soerenzig@gmail.com>
2018-10-05 16:45:40 +0200

    first commit

---
+++ Evalsys/Scripts/jsx/HelloWorld.jsx
@@ -0,0 +1,14 @@
+﻿var HelloWorld = React.createClass({
+    render: function () {
+        return (
+            <div> Hello {this.props.name} </div>
+        );
+
+    }
+
+});
+
+React.render(
+    <HelloWorld name="World" />,
+    document.getElementById('container')
+);
